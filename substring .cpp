b8c485756fd1e457c27c49073837cb56f1ef0d49
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2024-02-10 14:10:56 +0530

    Update substring .cpp

--- substring .cpp
+++ substring .cpp
@@ -10,8 +10,55 @@ void printSubstrings(std::string str) {
     }
 }
 
+
+// void printSubstrings(std::string str, std::string curr = "", int index = 0) { /// recursive approach 
+//     if(index == str.length()) {
+//         std::cout << curr << std::endl;
+//         return;
+//     }
+//     // Two recursive calls, one includes the character at current index, the other does not
+//     printSubstrings(str, curr, index + 1);
+//     printSubstrings(str, curr + str[index], index + 1);
+// }
+
 int main() {
     std::string str = "abc";
     printSubstrings(str);
     return 0;
 }
+
+
+
+
+
+// class Solution {  // leetcode 647
+// public:
+//     // Helper function to count the number of palindromes centered at s[i] and s[j]
+//     int palin(int i, int j, string &s){
+//         int cnt = 0;
+//         // Expand around the center and count the palindromes
+//         while(i >= 0 && j < s.length() && s[i] == s[j]){
+//             cnt++; // Increment the count for each palindrome
+//             i--; // Move to the left
+//             j++; // Move to the right
+//         }
+//         return cnt;
+//     }
+    
+//     int countSubstrings(string s) {
+//         int cnt = 0;
+//         // Iterate over the string
+//         for(int i = 0; i < s.length() ;i++){
+//            // For each character, consider it as the center of a palindrome
+//            int j = i;
+//            // Count the palindromes of odd length (centered at s[i])
+//            int oddLengthVale = palin(i,j , s);
+//            // Count the palindromes of even length (centered between s[i] and s[i+1])
+//            int evenLengthVale= palin(i , j+1 ,s);
+//            // Add the counts to the total count
+//            cnt += (oddLengthVale + evenLengthVale);
+//         }
+//         // Return the total count
+//         return cnt;
+//     }
+// };
