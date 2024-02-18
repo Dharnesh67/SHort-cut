70799ccdcba805352fb541d7737691ec36604d36
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2024-02-18 21:10:37 +0530

    Create Connected_component.cpp

---
+++ Connected_component.cpp
@@ -0,0 +1,8 @@
+  void f(vector<vector<char>>& grid,int i ,int j){
+        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j]!='1') return;
+        grid[i][j]='0';
+        f(grid,i+1,j);
+        f(grid,i,j+1);
+        f(grid,i-1,j);
+        f(grid,i,j-1);
+    }
