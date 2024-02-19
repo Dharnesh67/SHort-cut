6a8917b6c732bbc5598019bec950b9f17762982b
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2024-02-19 14:06:40 +0530

    Create reverse_linked_list.cpp

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,15 @@
+Node* reverseLinkedList(Node* head) {
+    Node* prev = nullptr;
+    Node* current = head;
+    Node* next = nullptr;
+
+    while (current != nullptr) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    // After the loop, 'prev' will be the new head of the reversed list
+    return prev;
+}
