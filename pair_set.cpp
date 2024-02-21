6190c6024d88a5a1deeba3f2c72becfef4de3b88
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2024-02-21 19:23:06 +0530

    Create pair_set.cpp

---
+++ pair_set.cpp
@@ -0,0 +1,10 @@
+struct pair_hash {
+    template <class T1, class T2>
+    size_t operator () (const pair<T1,T2> &p) const {
+        auto h1 = hash<T1>{}(p.first);
+        auto h2 = hash<T2>{}(p.second); 
+
+        return h1 ^ h2;  
+    }
+};
+unordered_set<pair<int, int>,pair_hash> visited;
