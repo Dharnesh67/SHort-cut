e163ac9ae71e82064b6457ba599863ff2114d25e
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2024-02-25 00:54:00 +0530

    Create open_page.js

---
+++ open_page.js
@@ -0,0 +1,15 @@
+function opennew(){ // to redirect to a new page
+    window.location.href = "r.html";
+}
+function openl(){ // to redirect to a new page
+    window.location.href = "index.html";
+}
+
+
+
+function opennew(){ // to redirect to a new page in new tab
+    window.open = "r.html";
+}
+function openl(){ // to redirect to a new page
+    window.open = "index.html";
+}
