19e712ddd2c605fa90687def127aec6604d3666e
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2024-06-04 18:46:24 +0530

    Update Binary.cpp

--- Binary.cpp
+++ Binary.cpp
@@ -1,16 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-void printbinary(int num){
-    for (int i = 10; i >=0; i--)
-    {
-        cout<<((num>>i)&1);
-    }
-    cout<<"\n";
-}
 int main(){
-    for (int i = 0; i < 5; i++)
+    int n ;
+    cin>>n;
+    for (int i = 5; i >=0; i--)
     {
-        printbinary(i);
+        cout<<((n&(1<<i))!=0); // this can use for check if the ith bit is set or not
+        
     }
-    
 }
