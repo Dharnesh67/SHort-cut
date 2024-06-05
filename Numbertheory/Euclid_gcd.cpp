5c93aabdd659562d38b20752b6cba152fd115d12
Dharmesh <raikwardharmesh67@gmail.com>
2024-06-05 14:55:46 +0530

    mfnc

--- Numbertheory/Euclid_gcd.cpp
+++ Numbertheory/Euclid_gcd.cpp
@@ -3,14 +3,25 @@ using namespace std;
 
 int gcd(int num1, int num2)
 {
-    if (num2 == 0)
+    if (num1 > num2)
     {
-        return num1;
+        if (num2 == 0)
+        {
+            return num1;
+        }
+        return gcd(num2, num1 % num2);
     }
-    return gcd(num2, num1 % num2);
+    else
+    {
+        if (num1 == 0)
+        {
+            return num2;
+        }
+        return gcd(num1, num2 % num1);
+    }
 }
 
 int main()
 {
-    cout << gcd(12, 4);
+    cout << gcd(4,12);
 }
