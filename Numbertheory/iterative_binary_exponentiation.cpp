9c2d565b095a369c4cc7b27516a217c4eb1acb6c
Dharmesh <raikwardharmesh67@gmail.com>
2024-06-07 12:02:46 +0530

    q;kpqf

--- Numbertheory/iterative_binary_exponentiation.cpp
+++ Numbertheory/iterative_binary_exponentiation.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main()
-{
+{ // TC : LOG(b)
     int a, b; // Declare variables a and b
     cin >> a >> b; // Input values for a and b from the user
 
+    cout<<pow(a,b)<<" ";
     int ans = 1; // Initialize the answer to 1
     while (b > 0) // Loop until b becomes 0
     {
         if (b & 1) // If b is odd
         {
             ans = ans * a; // Multiply ans by a
-            b -= 1; // Decrement b by 1 to make it even
         }
         a = a * a; // Square the value of a
         b = b >> 1; // Right shift b by 1 (equivalent to dividing by 2)
     }
-    cout << ans; // Output the final answer
+    cout << ans<<" "; // Output the final answer
 }
