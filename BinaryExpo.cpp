45ddc6fccaa9ae2fac477545c3a33251a4a4a86e
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2025-04-14 00:24:45 +0530

    Update BinaryExpo.cpp

--- BinaryExpo.cpp
+++ BinaryExpo.cpp
@@ -1,9 +1,10 @@
-ll BinaryExpo(ll a, ll b)
-{
-    if(b==0) return 1;
-    cout<<b<<" ";
-    if(b%2!=0){
-        return a*BinaryExpo(a,b/2)*BinaryExpo(a,b/2);
+ll BinaryExpo(ll a, ll b) {
+    if (b == 0)
+        return 1;
+    ll res = BinaryExpo(a, b / 2);
+    res = (res * res) % mod;
+    if (b % 2 != 0) {
+        res = (res * a) % mod;
     }
-    return BinaryExpo(a,b/2)*BinaryExpo(a,b/2);
+    return res;
 }
